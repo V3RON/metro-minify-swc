e04e1fc94c35363181e88943a23439d6065955ed
Szymon Chmal <szymonchmal@gmail.com>
2025-02-12 20:58:27 +0100

    feat: improve config parsing

--- src/index.ts
+++ src/index.ts
@@ -1,43 +1,42 @@
-import { minify as swcMinify } from '@swc/core';
+import { type JsMinifyOptions, minify as swcMinify, type TerserCompressOptions, type Output as SwcOutput, type JsFormatOptions, type TerserEcmaVersion } from '@swc/core';
 import type { MinifierOptions, MinifierResult } from 'metro-transform-worker';
 
-async function minify({
-  code,
-  reserved,
-  config,
-}: MinifierOptions): Promise<MinifierResult> {
-  try {
-    const result = await swcMinify(code, {
-      ...config,
-      mangle: {
-        toplevel: false,
-        ...(config.mangle ?? {}),
-        reserved: reserved as string[],
-      },
-      compress: {
-        reduce_funcs: false,
-        ...(config.compress ?? {}),
-      },
-      format: {
-        asciiOnly: true,
-        wrapIife: true,
-        ...(config.format ?? {}),
-      },
-      sourceMap: true,
-    });
+async function minifier(options: MinifierOptions): Promise<MinifierResult> {
+  const result = await minify(options);
 
-    return {
-      code: result.code,
-      map: result.map ? JSON.parse(result.map) : undefined,
-    };
-  } catch (error) {
-    throw new Error(
-      'Failed to minify code with SWC. This might be due to invalid configuration. ' +
-      'Please ensure you pass correct minifierConfig in your Metro configuration. ', {
-        cause: error,
-      },
-    );
+  if (!options.map || result.map == null) {
+    return {code: result.code};
   }
+
+  return { 
+    code: result.code, 
+    map: { 
+      ...JSON.parse(result.map),
+      sources: [options.filename] 
+    } 
+  };
 }
 
-export = minify;
+async function minify({ code, map, reserved, config }: MinifierOptions): Promise<SwcOutput> {
+  const { wrap_iife, ...output } = (config.output || {}) as  { wrap_iife: boolean | undefined };
+
+  const swcOptions: JsMinifyOptions = {
+    mangle: config.mangle === false ? false : {
+      ...(config.mangle || {}),
+      reserved: reserved as string[] || [],
+    },
+    format: {
+      ...output,
+      wrapIife: wrap_iife,
+    },
+    sourceMap: !!map && !!config.sourceMap,
+    toplevel: config.toplevel as boolean | undefined,
+    compress: config.compress as TerserCompressOptions,
+    module: config.module as boolean | undefined || false,
+    ecma: config.ecma as TerserEcmaVersion | undefined,
+  };
+
+  return await swcMinify(code, swcOptions);
+}
+
+export = minifier;
